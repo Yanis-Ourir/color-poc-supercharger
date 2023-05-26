52c04afbaa2ec3d4b47a46d700a3e2a04d52feb1
Yanis <ourir.yanis@gmail.com>
2023-05-26 11:33:05 +0200

    second  commit

--- content.tsx
+++ content.tsx
@@ -1,32 +1,57 @@
-import React, { createElement } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { Storage } from "@plasmohq/storage";
 
 export default function Content() {
+  const storage = new Storage();
 
-  const blueLink = document.getElementsByClassName('private-button__link'); // la couleur bleu des liens
+  const [buttonColor, setButtonColor] = useState("");
+  const [headerColor, setHeaderColor] = useState("");
+  const [linkColor, setLinkColor] = useState("");
+  const styleElement = document.createElement('style');
+
+  useEffect(() => {
+    async function changeColors() {
+      const storedButtonColor = await storage.get("colorPrincipal");
+      const storedHeaderColor = await storage.get("colorHeader");
+      const storedLinkColor = await storage.get("colorLink");
+
+      setButtonColor(storedButtonColor);
+      setHeaderColor(storedHeaderColor);
+      setLinkColor(storedLinkColor);
+
+    }
+
+    changeColors();
+  }, []); 
+
+  const links = document.querySelectorAll('.private-button__link');
+  const buttons = document.querySelectorAll('.private-button--primary');
+  const secondaryButtons = document.querySelectorAll('.private-button--secondary'); 
   
+  if (links.length > 0 && buttons.length > 0 && secondaryButtons.length > 0) {
+    const getColorValue = (element) => {
+      return window.getComputedStyle(element).getPropertyValue('color');
+    };
 
-  if (!blueLink) {
-    return;
+    const linksColorValue = getColorValue(links[0]);
+    const buttonColorValue = getColorValue(buttons[0]);
+    const secondaryButtonsColorValue = getColorValue(secondaryButtons[0]);
+
+    console.log(linksColorValue + "" + buttonColorValue + "" + secondaryButtonsColorValue);
+
   }
-  // => ou changer directement la couleur de la classe private-button__link ?
 
-  // avoir son style et changer sa couleur 
-  const testBtn = document.createElement("button");
-  testBtn.innerHTML = "Changer Couleur";
-  const getBody = document.querySelector("body");
-  console.log(getBody);
-  getBody.appendChild(testBtn);
+  styleElement.innerHTML = `
+          .private-button__link { color: ${linkColor}; } 
+          .private-button--primary { background-color: ${buttonColor}; border-color: ${buttonColor}} 
+          .private-button--secondary { color: ${buttonColor}; border-color: ${buttonColor}}
+          .navbar-inner { background-color: ${headerColor} }
+          #hs-nav-v4 .navSearch-v2 .navSearch-inputWrapper .navSearch-input { background-color: ${headerColor}}
+          #hs-nav-v4 .mobile .nav-left  { background: ${headerColor}}
+          #hs-nav-v4 .primary-links>li.active>a, #hs-nav-v4 .primary-links>li.currentPage>a  { background-color: ${headerColor}}
+        `;
+  document.head.appendChild(styleElement);
 
-  testBtn.addEventListener('click', () => {
-    // changer la couleur de la class private-button__link
-    const styleElement = document.createElement('style');
-    styleElement.innerHTML = '.private-button__link { color: #FF0000; }';
-    document.head.appendChild(styleElement);
-  })
-
-  return (
-    <div>content</div>
-  )
+  return;
 }
 
--- popup.tsx
+++ popup.tsx
@@ -5,40 +5,46 @@ import "./assets/css/styles.css";
 function IndexPopup() {
   const storage = new Storage();
 
-  const [colorPrincipal, setColorPrincipal] = useState("");
+  const [colorPrincipal, setColorPrincipal] = useState(""); // => couleur des bouttons;
   const [colorLink, setColorLink] = useState("");
   const [colorHeader, setColorHeader] = useState("");  // => Mettre les couleurs par défauts de hubspot que je vais récupérer sur une variable
-  const [colorHelp, setColorHelp] = useState("");
 
   useEffect(() => {
-    async function fetchData() {
-        const storedColorPrincipal = await storage.get("colorPrincipal");
-        const storedColorLink = await storage.get("colorLink");
-        const storedColorHeader = await storage.get("colorHeader");
-        const storedColorHelp = await storage.get("colorHelp");
+    async function storedColors() {
 
-        setColorPrincipal(storedColorPrincipal || "");
-        setColorLink(storedColorLink || "");
-        setColorHeader(storedColorHeader || ""); // => pareil ici pour les couleurs par défauts (stocker dans une variable)
-        setColorHelp(storedColorHelp || "");
-    } fetchData(); 
+
+      const storedColorPrincipal = await storage.get('colorPrincipal');
+      const storedColorLink = await storage.get('colorLink');
+      const storedColorHeader = await storage.get('colorHeader');
+
+
+      setColorPrincipal(storedColorPrincipal);
+      setColorLink(storedColorLink);
+      setColorHeader(storedColorHeader);
+
+    } storedColors();
   }, []);
 
+
+  console.log(colorHeader);
+  console.log(colorPrincipal);
+  console.log(colorLink);
 
   function handleColorChange(event, setColor) {
     const newColor = event.target.value;
     setColor(newColor);
-    console.log(newColor);
   }
+
 
   function handleSubmit(event) {
     event.preventDefault();
     storage.set("colorPrincipal", colorPrincipal);
     storage.set("colorLink", colorLink);
     storage.set("colorHeader", colorHeader);
-    storage.set("colorHelp", colorHelp);
-    // Envoyez les couleurs au content.js via un message peut-être
+    chrome.tabs.reload();
   }
+
+
 
   return (
     <div>
@@ -46,26 +52,23 @@ function IndexPopup() {
 
       <form onSubmit={handleSubmit}>
         <div className="color-div">
-          <label>Orange principal</label>
+          <label>Orange principal :</label>
           <input onChange={(e) => handleColorChange(e, setColorPrincipal)} type="color" id="colorpicker-principal" value={colorPrincipal} />
         </div>
 
         <div className="color-div">
-          <label>Bleu clair lien</label>
+          <label>Bleu clair lien :</label>
           <input onChange={(e) => handleColorChange(e, setColorLink)} type="color" id="colorpicker-link" value={colorLink} />
         </div>
 
         <div className="color-div">
-          <label>Bleu foncé header</label>
+          <label>Bleu foncé header :</label>
           <input onChange={(e) => handleColorChange(e, setColorHeader)} type="color" id="colorpicker-header" value={colorHeader} />
         </div>
 
-        <div className="color-div">
-          <label>Bleu aide</label>
-          <input onChange={(e) => handleColorChange(e, setColorHelp)} type="color" id="colorpicker-help" value={colorHelp} />
+        <div className="div-color-button">
+          <button className="color-button" type="submit">Save</button>
         </div>
-
-        <button type="submit">Save</button>
       </form>
     </div>
   );
